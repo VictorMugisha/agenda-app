10dbe54deb1798917bcdbfb6222e2687c418b1ed
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-20 10:47:06 +0200

    Installed cookie-parser and created protected routes

---
+++ server/middlewares/protect.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.model.js";
+
+export default async function protectedRoute(req, res, next) {
+  try {
+    const token = req.cookies.agenda_token;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No token found. Authorization denied!" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Invalid token!" });
+    }
+
+    const user = await UserModel.findById(decoded.userId).select("-password");
+    
+    req.loggedInUser = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectedRoute middleware: ", error);
+    return res.status(500).json("Internal server error");
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import protectedRoute from "./middlewares/protect.middleware.js";
 
 dotenv.config();
 connectDB();
@@ -12,11 +14,12 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
+app.use("/users", protectedRoute, userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
