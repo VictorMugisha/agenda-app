5cfd1f6ec3f0a2a1b398f1f1925cf2047a02b256
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-20 09:40:38 +0200

    Logout user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -111,3 +111,18 @@ export async function loginUser(req, res) {
     return res.status(500).json({ message: "Internal server error" });
   }
 }
+
+export async function logoutUser(req, res) {
+  try {
+    res.clearCookie("agenda_token", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ message: "Successfully logged out" });
+  } catch (error) {
+    console.log("Error in logoutUser controller: ", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
