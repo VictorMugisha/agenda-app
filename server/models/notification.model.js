de6102241c154ad27cd19f7f9009080764c91c26
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-27 11:47:34 +0200

    Create Notification model

---
+++ server/models/notification.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      reqired: true,
+    },
+    content: {
+      type: String,
+      reqired: true,
+    },
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    recipients: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
