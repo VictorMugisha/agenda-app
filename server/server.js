6730bb48fd1a46a8dc768877bbfcf3a1ea69fd1b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-14 18:20:59 +0200

    Update the cors allowed endpoints in server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "*",
   })
 );
 
