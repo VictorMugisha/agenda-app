96e2efb5bdc5a700a16141abfc0d3f9443be72a9
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-15 11:25:12 +0200

    troubleshot: using import.meta.env.MODE in global constants file

--- server/client/src/constants/constants.js
+++ server/client/src/constants/constants.js
@@ -1,5 +1,6 @@
 export const BACKEND_URL = "http://localhost:8000";
 export const CLOUD_NAME = "victormugisha";
 export const UPLOAD_PRESET = "agenda_app";
-// export const ENVIRONMENT = "development";
-export const ENVIRONMENT = "production";
+export const ENVIRONMENT = import.meta.env.MODE === "development" ? "development" : "production";
+console.log("The current working environment: ", ENVIRONMENT);
+
