115045ae8deb9e73bd78ffea385961e3b87f9fbf
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-15 09:58:42 +0200

    troubleshot: update package.json build script

--- server/client/src/socket.js
+++ server/client/src/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://victor-agenda-app.onrender.com";
 
+console.log("Current environment:", import.meta.env.MODE);
 console.log("SOCKET_URL:", SOCKET_URL);
 
 const socket = io(SOCKET_URL, {
